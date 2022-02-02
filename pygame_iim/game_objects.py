a1421bbdda921d863f8870f9775284eaf4b83c63
IdoLior99 <35223557+IdoLior99@users.noreply.github.com>
2022-02-02 16:23:49 +0200

    Finished Main menu, starting sounds
    Animated Sprite is pre-abstraction

--- pygame_iim/game_objects.py
+++ pygame_iim/game_objects.py
@@ -1,17 +1,50 @@
 import pygame
+import glob
 
 
-# TODO- draw button sprites
-class Button(pygame.sprite.Sprite):
-    def __init__(self, idle_img_path, pressed_img_path):
+# TODO - Make Animated_Sprite class abstract, create Button ,Npc and Player classes appropriately, should look better.
+# Toggle between them as the right direction key is pressed.
+# In case of an animated button - define the 'sensitive zone'? and find a way to get the button's coordinates.
+
+class Animated_Sprite(pygame.sprite.Sprite):
+    def __init__(self, animation_path, speed, pos_x, pos_y, game_size, stationary=True, interactable=True, img_format='png', flip=False):
         super().__init__()
-        self.image = pygame.image.load(idle_img_path)
-        self.alt_image = pygame.image.load(pressed_img_path)
+        self.sprites = []
+        for f in glob.glob(animation_path + '/*.' + img_format):
+            img = pygame.image.load(f)
+            img = pygame.transform.smoothscale(img, game_size)
+            self.sprites.append(img)
+        self.current_sprite = 0
+        self.speed = speed
+        self.key_pressed = False
+        self.key_released = False
+        self.image = self.sprites[self.current_sprite]
         self.rect = self.image.get_rect()
-        self.alt_rect = self.alt_image.get_rect()
+        self.rect.center = [pos_x, pos_y]
+
+        # Button = stat+inter, Player = !stat+!inter, Npc = !stat + inter, Background Torch = stat + !inter :
+        self.is_stat = stationary
+        self.is_inter = interactable
+
+    def set_pressed(self):
+        self.key_pressed = True
+        self.key_released = False
+
+    def set_released(self):
+        self.key_released = True
+        self.key_pressed = False
 
     def update(self):
-        temp = self.rect    
-        self.rect = self.alt_rect
-        self.alt_rect = temp
+        if self.key_pressed and self.current_sprite == 0:
+            self.current_sprite += self.speed
 
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                # self.is_animating = False
+        if self.key_released:
+            self.current_sprite = 0
+        self.image = self.sprites[int(self.current_sprite)]
+
+    def coll_check(self, event_pos):
+        return (event_pos[0] in range(self.rect.center[0]-100, self.rect.center[0]+100)) and \
+               (event_pos[1] in range(self.rect.center[1]-50, self.rect.center[1]+50))
--- pygame_iim/main.py
+++ pygame_iim/main.py
@@ -1,16 +1,21 @@
 import pygame
 from operator import add
+from game_objects import *
 
 
-def start_setup(game_dims, game_name, icon_path, bg_path):
-    home_screen = pygame.display.set_mode(game_dims)
-
-    pygame.display.set_caption(game_name)
-    icon = pygame.image.load(icon_path)
+def fit_bg_dims(game_dims, bg_path):
     bg = pygame.image.load(bg_path).convert()  # Tutorial guy said it's important
     bg = pygame.transform.smoothscale(bg, game_dims)  # Changes image dims
+    return bg
+
+
+def game_setup(game_dims, game_name, icon_path, bg_path):
+
+    screen = pygame.display.set_mode(game_dims)
+    pygame.display.set_caption(game_name)
+    icon = pygame.image.load(icon_path)
     pygame.display.set_icon(icon)
-    return home_screen, bg
+    return screen, fit_bg_dims(game_dims, bg_path)
 
 
 def player(h_screen, p_img, p_coords):
@@ -28,8 +33,8 @@ def border_check(game_dims, p_coords, player_size):
         p_coords[1] = game_dims[1] - player_size
 
 
-def move_player(delts, speed):
-    if event.type == pygame.KEYDOWN:
+def move_player(delts, speed, down=True):
+    if down:
         if event.key == pygame.K_a:
             delts[0] -= speed
         if event.key == pygame.K_d:
@@ -38,7 +43,7 @@ def move_player(delts, speed):
             delts[1] -= speed
         if event.key == pygame.K_s:
             delts[1] += speed
-    if event.type == pygame.KEYUP:
+    else:
         if event.key == pygame.K_a or event.key == pygame.K_d:
             delts[0] = 0
         if event.key == pygame.K_w or event.key == pygame.K_s:
@@ -47,21 +52,55 @@ def move_player(delts, speed):
 
 pygame.init()
 game_size = (800, 600)
-home_screen, bg = start_setup(game_size, 'Tomidos project', 'game_images/monster.png', 'game_images/hm_bg.png')
-player_img = pygame.image.load('game_images/hm_player_core.png')
+core_screen, main_menu = game_setup(game_size, 'Tomidos project', 'game_images/monster.png', 'game_images/main_menu.png')
+player_img = pygame.image.load('game_images/player/hm_player_core.png')
 player_img = pygame.transform.smoothscale(player_img, (161, 107))
 player_coords = [370, 480]
+
 # The Game Loop
 running = True
 deltas = [0, 0]
+buttons = pygame.sprite.Group()
+play_button = Animated_Sprite('game_images/play_button', 1, 400, 200, (215, 162))
+quit_button = Animated_Sprite('game_images/quit_button', 1, 400, 400, (215, 162))
+buttons.add(play_button)
+buttons.add(quit_button)
+
+level_0 = fit_bg_dims(game_size, 'game_images/hm_bg.png')
+curr_screen = main_menu
+
 while running:
-    home_screen.blit(bg, (0, 0))
+    core_screen.blit(curr_screen, (0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        move_player(deltas, 0.45)
+        if event.type == pygame.KEYDOWN:
+            move_player(deltas, 1)
+        if event.type == pygame.KEYUP:
+            move_player(deltas, 1, down=False)
+        if event.type == pygame.MOUSEMOTION:
+            if play_button.coll_check(event.pos):
+                play_button.set_pressed()
+            else:
+                play_button.set_released()
+            if quit_button.coll_check(event.pos):
+                quit_button.set_pressed()
+            else:
+                quit_button.set_released()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if quit_button.coll_check(event.pos) and curr_screen == main_menu:
+                running = False
+            elif play_button.coll_check(event.pos) and curr_screen == main_menu:
+                curr_screen = level_0
+
     player_coords = list(map(add, player_coords, deltas))  # should be time efficient
     border_check(game_size, player_coords, 32)
-    player(home_screen, player_img, player_coords)
+    if curr_screen == level_0:
+        player(core_screen, player_img, player_coords)
+    if curr_screen == main_menu:
+        buttons.draw(core_screen)
+        buttons.update()
+
     pygame.display.update()  # TODO bad for perfomance, should keep a list of objects(rects/sprites) that are updated and only update them
